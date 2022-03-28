004793354c88f7faa7cf454ec2ab8ae80177ff0c
Anastasia <anastasiaputeeya@gmail.com>
2022-03-28 11:40:02 +0300

    Fixed some bugs

--- app/js/script.js
+++ app/js/script.js
@@ -19,6 +19,33 @@ function displayDots() {
 	slickDots.style.zIndex = 'auto';
 }
 
+window.onload = function () {
+	var search = document.querySelector("#form-search");
+	if (search) {
+		search.addEventListener('keyup', showButton, true);
+		search.addEventListener('click', resetInput, true);
+		search.addEventListener('click', showButton, true);
+	}
+}
+
+function resetInput(e) {
+	var input = this.querySelector('#search');
+	input.value = input.defaultValue;
+}
+
+function showButton() {}
+
+function addClass(o, c) {
+	var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
+	if (re.test(o.className)) return
+	o.className = (o.className + " " + c).replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+}
+
+function removeClass(o, c) {
+	var re = new RegExp("(^|\\s)" + c + "(\\s|$)", "g")
+	o.className = o.className.replace(re, "$1").replace(/\s+/g, " ").replace(/(^ | $)/g, "")
+}
+
 
 $(document).ready(function () {
 	var promoSwiper = new Swiper('#promo-slider', {
